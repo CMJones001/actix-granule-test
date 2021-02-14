746965eb8d5a61035905b4b22719b9112763a345
Carl Jones <c.m.jones001@gmail.com>
2021-02-14 17:57:08 +0100

    [feat] Basic integration test

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ mod errors;
 mod handler;
 mod models;
 
+use actix_rt;
 use actix_web::{App, HttpServer};
 use dotenv::dotenv;
 use slog::{info, o, Drain, Logger};
@@ -18,7 +19,7 @@ fn configure_log() -> Logger {
     slog::Logger::root(console_drain, o!("v" => env!("CARGO_PKG_VERSION")))
 }
 
-#[actix_web::main]
+#[actix_rt::main]
 async fn main() -> std::io::Result<()> {
     // Set up the configuration
     dotenv().ok();
@@ -50,4 +51,36 @@ async fn main() -> std::io::Result<()> {
     .bind(format!("{}:{}", config.server.host, config.server.port))?
     .run()
     .await
+}
+
+#[cfg(test)]
+mod integratiotests {
+    use super::*;
+    use actix_web::test;
+    use models::AppState;
+    use serde_json;
+
+    #[actix_rt::test]
+    async fn test_response() {
+        dotenv().ok();
+        let config = crate::config::Config::from_env().unwrap();
+        let log = configure_log();
+        let pool = config.pg.create_pool(NoTls).unwrap();
+
+        let app_state = models::AppState { pool, log };
+        let app = App::new().data(app_state).service(handler::get_experiments);
+        let mut app = test::init_service(app).await;
+
+        let req = test::TestRequest::get().uri("/exp/").to_request();
+        let response = test::call_service(&mut app, req).await;
+
+        assert_eq!(response.status(), 200, "GET /Exp should return 200");
+
+        let body = test::read_body(response).await;
+
+        let try_experiments: Result<Vec<models::Experiment>, serde_json::error::Error> =
+            serde_json::from_slice(&body);
+
+        assert!(try_experiments.is_ok(), "Response could not be parsed");
+    }
 }
